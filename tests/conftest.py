6f4a3026699c54f5ead5a1fb6b491286ce22984d
Pablo Tamarit <pablo.tamarit@cern.ch>
2025-02-21 15:35:27 +0100

    tests: username with a dash

--- tests/conftest.py
+++ tests/conftest.py
@@ -156,7 +156,7 @@ def users_data():
     """Data for users."""
     return [
         {
-            "username": "pubres",
+            "username": "pub-res",
             "email": "pubres@inveniosoftware.org",
             "profile": {
                 "full_name": "Tim Smith",
@@ -367,7 +367,7 @@ def user_accented(users):
 @pytest.fixture(scope="module")
 def user_pubres(users):
     """User tjs (public/restricted)."""
-    return users["pubres"]
+    return users["pub-res"]
 
 
 @pytest.fixture(scope="module")
